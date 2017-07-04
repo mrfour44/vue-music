ac5e384d60c3f55bbc7f9ad59c2c7cd74553eb77
mrfour44 <magiczi@qq.com>
2017-07-04 19:46:59 +0800

    歌手详情添加播放器内核vuex状态

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,7 +1,6 @@
 <template>
     <transition name="slide">
-        <div class="singer-detail">
-        </div>
+      <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
     </transition>
 </template>
 <script>
@@ -9,6 +8,7 @@ import {mapGetters} from 'vuex'
 import {getSingerDetail} from '@/api/singer'
 import {ERR_OK} from '@/api/config'
 import {createSong} from '@/common/js/song'
+import musicList from '@/components/music-list/music-list'
 export default {
     data() {
         return {
@@ -16,6 +16,12 @@ export default {
         }
     },
     computed: {
+        title() {
+            return this.singer.name
+        },
+        bgImage() {
+            return this.singer.avatar
+        },
         ...mapGetters([
             'singer'
         ])
@@ -46,21 +52,14 @@ export default {
             })
             return ret
         }
+    },
+    components: {
+        musicList
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-    @import '../../common/stylus/variable'
-
-    .singer-detail
-        position: fixed
-        z-index: 100
-        top: 0
-        left: 0
-        right: 0
-        bottom: 0
-        background: $color-background
     .slide-enter-active, .slide-leave-active
         transition: all 0.3s
     .slide-enter, .slide-leave-to
