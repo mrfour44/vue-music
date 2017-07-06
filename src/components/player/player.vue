dbe0e91cdce16d79bced50bf1cf8e39160214429
mrfour44 <magiczi@qq.com>
2017-07-06 18:57:46 +0800

    播放器内核

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,17 +1,216 @@
 <template>
     <div class="player" v-show="playlist.length>0">
-        <div class="normal-player" v-show="fullScreen">播放器</div>
-        <div class="mini-player" v-show="!fullScreen"></div>
+        <transition name="normal" @enter="enter" @after-enter="afterEnter" @leave="leave" @after-leave="afterLeave">
+            <div class="normal-player" v-show="fullScreen">
+                <div class="background">
+                    <img width="100%" height="100%" :src="currentSong.image">
+                </div>
+                <div class="top">
+                    <div class="back" @click="back">
+                        <i class="icon-back"></i>
+                    </div>
+                    <h1 class="title" v-html="currentSong.name"></h1>
+                    <h2 class="subtitle" v-html="currentSong.singer"></h2>
+                </div>
+                <div class="middle">
+                    <div class="middle-l">
+                        <div class="cd-wrapper" ref="cdWrapper">
+                            <div class="cd" :class="cdCls">
+                                <img class="image" :src="currentSong.image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="operators">
+                        <div class="icon i-left">
+                            <i class="icon-sequence"></i>
+                        </div>
+                        <div class="icon i-left">
+                            <i @click="prev" class="icon-prev"></i>
+                        </div>
+                        <div class="icon i-center">
+                            <i @click="togglePlaying" :class="playIcon"></i>
+                        </div>
+                        <div class="icon i-right">
+                            <i @click="next" class="icon-next"></i>
+                        </div>
+                        <div class="icon i-right">
+                            <i class="icon icon-not-favorite"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+        <transition name="mini">
+            <div class="mini-player" v-show="!fullScreen" @click="open">
+                <div class="icon">
+                    <img :class="cdCls" width="40" height="40" :src="currentSong.image">
+                </div>
+                <div class="text">
+                    <h2 class="name" v-html="currentSong.name"></h2>
+                    <p class="desc" v-html="currentSong.singer"></p>
+                </div>
+                <div class="control">
+                    <i @click.stop="togglePlaying" :class="miniIcon"></i>
+                </div>
+                <div class="control">
+                    <i class="icon-playlist"></i>
+                </div>
+            </div>
+        </transition>
+        <audio :src="currentSong.url" ref="audio" @canplay="ready" @error="error"></audio>
     </div>
 </template>
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
+import animations from 'create-keyframe-animation' // 用js创建css3的animation
+import { prefixStyle } from '@/common/js/dom'
+
+const transform = prefixStyle('transform')
 export default {
+    data() {
+        return {
+            songReady: false
+        }
+    },
     computed: {
+        playIcon() {
+            return this.playing ? 'icon-pause' : 'icon-play'
+        },
+        miniIcon() {
+            return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+        },
+        cdCls() {
+            return this.playing ? 'play' : 'play pause'
+        },
         ...mapGetters([
             'fullScreen',
-            'playlist'
+            'playlist',
+            'currentSong',
+            'playing',
+            'currentIndex'
         ])
+    },
+    methods: {
+        back() {
+            this.setFullScreen(false)
+        },
+        open() {
+            this.setFullScreen(true)
+        },
+        enter(el, done) {
+            const { x, y, scale } = this._getPostAndScale()
+            let animation = {
+                0: {
+                    transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+                },
+                60: {
+                    transform: `translate3d0, 0, 0) scale(2)`
+                },
+                100: {
+                    transform: `translate3d0, 0, 0) scale(1)`
+                }
+            }
+
+            animations.registerAnimation({
+                name: 'move',
+                animation,
+                presets: {
+                    duration: 400,
+                    easing: 'linear'
+                }
+            })
+            animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+        },
+        afterEnter() {
+            animations.unregisterAnimation('move')
+            this.$refs.cdWrapper.style.animation = ''
+        },
+        leave(el, done) {
+            this.$refs.cdWrapper.style.transition = 'all 0.4s'
+            const { x, y, scale } = this._getPostAndScale()
+            this.$refs.cdWrapper.style[transform] = `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+            this.$refs.cdWrapper.addEventListener('transitionend', done)
+        },
+        afterLeave() {
+            this.$refs.cdWrapper.style.transition = ''
+            this.$refs.cdWrapper.style[transform] = ''
+        },
+        togglePlaying() {
+            if (!this.songReady) {
+                return
+            }
+            this.setPlayingState(!this.playing)
+        },
+        prev() {
+            if (!this.songReady) {
+                return
+            }
+            let index = this.currentIndex - 1
+            if (index === -1) {
+                index = this.playlist.length - 1
+            }
+            this.setCurrentIndex(index)
+            // 如果暂停情况 切下一首歌 需要调togglePlaying
+            if (!this.playing) {
+                this.togglePlaying()
+            }
+            this.songReady = false
+        },
+        next() {
+            if (!this.songReady) {
+                return
+            }
+            let index = this.currentIndex + 1
+            if (index === this.playlist.length) {
+                index = 0
+            }
+            this.setCurrentIndex(index)
+            if (!this.playing) {
+                this.togglePlaying()
+            }
+            this.songReady = false
+        },
+        ready() {
+            this.songReady = true
+        },
+        error() {
+            // this.songReady = true
+        },
+        _getPostAndScale() {
+            const targetWidth = 40
+            const paddingLeft = 40
+            const paddingBottom = 30
+            const paddingTop = 80
+            const width = window.innerWidth * 0.8
+            const scale = targetWidth / width
+            const x = -(window.innerWidth / 2 - paddingLeft)
+            const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+            return {
+                x,
+                y,
+                scale
+            }
+        },
+        ...mapMutations({
+            setFullScreen: 'SET_FULL_SCREEN',
+            setPlayingState: 'SET_PLAYING_STATE',
+            setCurrentIndex: 'SET_CURRENT_INDEX'
+        })
+    },
+    watch: {
+        currentSong() {
+            this.$nextTick(() => {
+                this.$refs.audio.play()
+            })
+        },
+        playing(newPlaying) {
+            const audio = this.$refs.audio
+            this.$nextTick(() => {
+                newPlaying ? audio.play() : audio.pause()
+            })
+        }
     }
 }
 </script>
@@ -20,7 +219,7 @@ export default {
     @import '../../common/stylus/mixin'
     .player
         .normal-player
-            position: fixed
+            position: fixed  
             left: 0
             right: 0
             top: 0
@@ -194,7 +393,7 @@ export default {
                 transition: all 0.4s
                 .top, .bottom
                     transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
-            &.normal-enter, .&.normal-leave-to
+            &.normal-enter, &.normal-leave-to
                 opacity: 0
                 .top
                     transform: translate3d(0, -100px, 0)
@@ -212,7 +411,7 @@ export default {
             background: $color-highlight-background
             &.mini-enter-active, &.mini-leave-active
                 transition: all 0.4s
-            &.mini-enter, .&.mini-leave-to
+            &.mini-enter, &.mini-leave-to
                 opacity: 0
             .icon
                 flex: 0 0 40px
