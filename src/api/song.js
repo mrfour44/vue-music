4e1d69b50be9524146b0a35821b9d726a628674b
mrfour44 <magiczi@qq.com>
2017-08-03 10:01:41 +0800

    playMode

---
+++ src/api/song.js
@@ -0,0 +1,14 @@
+import { commonParams } from '@/api/config'
+import { axios } from 'axios'
+export function getLyric(mid) {
+    const url = '/api/lyric'
+    const data = Object.assign({}, commonParams, {
+        songmid: mid,
+        pcachetime: +new Date(),
+        platform: 'yqq',
+        hostUin: 0,
+        needNewCode: 0,
+        g_tk: 67232076,
+        format: 'json'
+    })
+}
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -30,8 +30,8 @@
                         <span class="time time-r">{{format(currentSong.duration)}}</span>
                     </div>
                     <div class="operators">
-                        <div class="icon i-left">
-                            <i class="icon-sequence"></i>
+                        <div class="icon i-left" @click="changeMode">
+                            <i :class="iconMode"></i>
                         </div>
                         <div class="icon i-left" :class="disableCls">
                             <i @click="prev" class="icon-prev"></i>
@@ -68,7 +68,7 @@
                 </div>
             </div>
         </transition>
-        <audio :src="currentSong.url" ref="audio" @canplay="ready" @error="error" @timeupdate="updateTime"></audio>
+        <audio :src="currentSong.url" ref="audio" @canplay="ready" @error="error" @timeupdate="updateTime" @ended="end"></audio>
     </div>
 </template>
 <script>
@@ -77,6 +77,8 @@ import animations from 'create-keyframe-animation' // 用js创建css3的animatio
 import { prefixStyle } from '@/common/js/dom'
 import ProgressBar from '@/base/progress-bar/progress-bar'
 import ProgressCircle from '@/base/progress-circle/progress-circle'
+import { playMode } from '@/common/js/config'
+import { shuffle } from '@/common/js/util'
 
 const transform = prefixStyle('transform')
 export default {
@@ -90,6 +92,9 @@ export default {
     computed: {
         playIcon() {
             return this.playing ? 'icon-pause' : 'icon-play'
+        },
+        iconMode() {
+            return this.mode === playMode.sequence ? 'icon-sequence' : this.mode === playMode.loop ? 'icon-loop' : 'icon-random'
         },
         miniIcon() {
             return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
@@ -108,7 +113,9 @@ export default {
             'playlist',
             'currentSong',
             'playing',
-            'currentIndex'
+            'currentIndex',
+            'mode',
+            'sequenceList'
         ])
     },
     methods: {
@@ -162,6 +169,17 @@ export default {
             }
             this.setPlayingState(!this.playing)
         },
+        end() {
+            if (this.mode === playMode.loop) {
+                this.loop()
+            } else {
+                this.next()
+            }
+        },
+        loop() {
+            this.$refs.audio.currentTime = 0
+            this.$refs.audio.play()
+        },
         prev() {
             if (!this.songReady) {
                 return
@@ -213,6 +231,24 @@ export default {
                 this.togglePlaying()
             }
         },
+        changeMode() {
+            const mode = (this.mode + 1) % 3
+            this.setPlayMode(mode)
+            let list = null
+            if (mode === playMode.random) {
+                list = shuffle(this.sequenceList)
+            } else {
+                list = this.sequenceList
+            }
+            this._resetCurrentIndex(list)
+            this.setPlayList(list)
+        },
+        _resetCurrentIndex(list) {
+            let index = list.findIndex((item) => {
+                return item.id === this.currentSong.id
+            })
+            this.setCurrentIndex(index)
+        },
         _pad(num, n = 2) {
             let len = num.toString().length
             while (len < n) {
@@ -239,11 +275,16 @@ export default {
         ...mapMutations({
             setFullScreen: 'SET_FULL_SCREEN',
             setPlayingState: 'SET_PLAYING_STATE',
-            setCurrentIndex: 'SET_CURRENT_INDEX'
+            setCurrentIndex: 'SET_CURRENT_INDEX',
+            setPlayMode: 'SET_PLAY_MODE',
+            setPlayList: 'SET_PLAYLIST'
         })
     },
     watch: {
-        currentSong() {
+        currentSong(newSong, oldSong) {
+            if (newSong.id === oldSong.id) {
+                return
+            }
             this.$nextTick(() => {
                 this.$refs.audio.play()
             })
