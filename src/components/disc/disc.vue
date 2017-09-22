b5476d9dbdc7728df65d3c88cdc6045f7dee2857
KrisLam <magiczi@qq.com>
2017-09-22 19:21:14 +0800

    update
    update -- 歌单数据处理

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,6 +1,6 @@
 <template>
     <transition name="slide">
-        <music-list :title="title" :bg-image="bgImage"></music-list>
+        <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
     </transition>
 </template>
 <script>
@@ -8,7 +8,13 @@ import MusicList from '@/components/music-list/music-list'
 import { mapGetters } from 'vuex'
 import { getSongList } from '@/api/recommend'
 import { ERR_OK } from '@/api/config'
+import { createSong } from '@/common/js/song'
 export default {
+    data() {
+        return {
+            songs: []
+        }
+    },
     computed: {
         title() {
             return this.disc.dissname
@@ -30,9 +36,19 @@ export default {
         _getSongList() {
             getSongList(this.disc.dissid).then((res) => {
                 if (res.code === ERR_OK) {
-                    console.log(res.cdlist[0].songlist)
+                    // console.log(res.cdlist[0].songlist)
+                    this.songs = this._normalizeSongs(res.cdlist[0].songlist)
                 }
             })
+        },
+        _normalizeSongs(list) {
+            let ret = []
+            list.forEach((musicData) => {
+                if (musicData.songid && musicData.albummid) {
+                    ret.push(createSong(musicData))
+                }
+            })
+            return ret
         }
     }
 }
